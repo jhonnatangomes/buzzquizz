6e2f93ba9f39f9ee505411c92bbe00018f387a19
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-13 13:39:09 -0300

    Opening quizzes when clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const URL_API = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes"
+const URL_API = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes";
 
 function getQuizzes() {
     const promise = axios.get(URL_API);
@@ -7,13 +7,14 @@ function getQuizzes() {
 
 function showQuizzes(response) {
     const quizzes = document.querySelector(".all-quizzes .quizzes");
+    
     quizzes.innerHTML = "";
     ids = JSON.parse(localStorage.getItem("ids"));
 
     for (let i = 0; i < response.data.length; i++){ 
         if(!ids.includes(response.data[i].id)) {
             quizzes.innerHTML += `
-            <div class="quizz" onclick="openQuizzPage();">
+            <div class="quizz">
                 <img src="${response.data[i].image}" alt="${response.data[i].title}">
                 <div>
                     ${response.data[i].title}
@@ -21,13 +22,52 @@ function showQuizzes(response) {
             </div>
             `
         }
-        
     }
+
+    const quizzArray = document.querySelectorAll(".all-quizzes .quizz");
+    for(let i = 0; i < quizzArray.length; i++) {
+        quizzArray[i].addEventListener('click', function () {
+            openQuizzPage(response.data[i].image, response.data[i].title, response.data[i].questions);
+        })
+    }
+
 }
 
-function openQuizzPage() {
+function openQuizzPage(image, title, questions) {
     const quizzesList = document.querySelector(".quizzes-list");
     const quizzPage = document.querySelector(".quizz-page");
+    let question;
+
+    quizzPage.innerHTML = "";
+
+    quizzPage.innerHTML += `
+    <div class="quizz-title">
+        <img src="${image}" alt="">
+        <span>${title}</span>
+    </div>
+    `
+    
+    for(let i = 0; i < questions.length; i++) {
+        quizzPage.innerHTML += `<section class="question">
+        <div class="question-header">
+            ${questions[i].title}
+        </div>
+
+        <div class="question-items">
+        </div>
+        </section>
+        `
+        question = document.querySelectorAll(".quizz-page .question .question-items")[i];
+        for(let j = 0; j < questions[i].answers.length; j++){
+            question.innerHTML += `    
+            <div class="question-item">
+                <img src="${questions[i].answers[j].image}" alt="">
+                <p>${questions[i].answers[j].text}</p>
+            </div>
+            `
+        }
+        
+    }
 
     quizzesList.classList.add("hidden");
     quizzPage.classList.remove("hidden");
