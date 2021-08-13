1bc074481c5dec2a1a7c452337d955ca7888c085
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-13 15:09:00 -0300

    Selecting correct quizz answer when clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const URL_API = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes";
-let quiz;
+let isAnswered = false, questionIdPrevious = 0;
 
 function getQuizzes() {
     const promise = axios.get(URL_API);
@@ -8,13 +8,15 @@ function getQuizzes() {
 
 function showQuizzes(response) {
     const quizzes = document.querySelector(".all-quizzes .quizzes");
-    console.log(response);
-    quiz = response;
     quizzes.innerHTML = "";
-    //const ids = JSON.parse(localStorage.getItem("ids"));
+    let ids = JSON.parse(localStorage.getItem("ids"));
+    
+    if(ids === null) {
+        ids = [];
+    }
 
     for (let i = 0; i < response.data.length; i++){
-        //f(ids !== null && !ids.includes(response.data[i].id)) {
+        if(!ids.includes(response.data[i].id)) {
             quizzes.innerHTML += `
             <div class="quizz">
                 <img src="${response.data[i].image}" alt="${response.data[i].title}">
@@ -23,7 +25,7 @@ function showQuizzes(response) {
                 </div>
             </div>
             `
-        //} 
+        } 
     }
 
     const quizzArray = document.querySelectorAll(".all-quizzes .quizz");
@@ -55,7 +57,7 @@ function openQuizzPage(image, title, questions) {
             ${questions[i].title}
         </div>
 
-        <div class="question-items">
+        <div class="question-items" id="question{${i + 1}}">
         </div>
         </section>
         `
@@ -64,7 +66,7 @@ function openQuizzPage(image, title, questions) {
         question = document.querySelectorAll(".quizz-page .question .question-items")[i];
         for(let j = 0; j < questions[i].answers.length; j++){
             question.innerHTML += `    
-            <div class="question-item">
+            <div class="question-item" onclick="selectAnswer(this, ${i+1});">
                 <img src="${questions[i].answers[j].image}" alt="">
                 <p>${questions[i].answers[j].text}</p>
             </div>
@@ -90,6 +92,28 @@ function shuffle(array) {
         const j = Math.floor(Math.random() * (i+1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+}
+
+function selectAnswer(element, questionId) {
+    console.log("Question ID: " + questionId);
+    if(questionId !== questionIdPrevious) {
+        isAnswered = false;
+    }
+
+    if(!isAnswered) {
+        element.classList.add("correct-item");
+        const items = element.parentNode.querySelectorAll(".question-item");
+        for (let i = 0; i < items.length; i++){
+            if(!items[i].classList.contains("correct-item")) {
+                items[i].classList.add("incorrect-item");
+            }
+        }
+        questionIdPrevious = questionId;
+
+        isAnswered = true;
+    }
+
+    
 }
 
 getQuizzes();
