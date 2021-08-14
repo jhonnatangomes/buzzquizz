373bf6dea13953cc28139a776246bfc70412daf0
Carlos Gabriel <carlosgabrielpc@live.com>
2021-08-14 03:39:17 -0300

    Checking title basic info page added

--- scripts/quizz-creation.js
+++ scripts/quizz-creation.js
@@ -4,11 +4,20 @@ const sectionQuizzQuestions = document.querySelector(".quizz-questions");
 const sectionQuizzLevels = document.querySelector(".quizz-levels");
 const sectionQuizzSuccess = document.querySelector(".quizz-success");
 
-function updatePage(page) {
+function changePage(page) {
     document.querySelectorAll("main section").forEach(e => {e.classList.add("hidden")});
     window.scrollTo({top: 0, left: 0, behavior: 'auto'});
     mainQuizzCreate.classList.remove("hidden");
     page.classList.remove("hidden");
 }
 
-updatePage(sectionBasicInfo);
+function checkBasicInfo() {
+    const titleLength = sectionBasicInfo.querySelector(".your-quizz-title").value.length;
+    if (titleLength >= 20 && titleLength <= 65) {
+        return "É valido.";
+    } else {
+        sectionBasicInfo.querySelector(".title-error").classList.remove("hidden");
+    }
+}
+
+changePage(sectionBasicInfo);
