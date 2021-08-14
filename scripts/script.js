41c8a2590a3595024eeee53488d174f2087e7787
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-13 23:35:43 -0300

    Corrected behavior when clicking answers and added smooth scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const URL_API = "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes";
-let isAnswered = false, questionIdPrevious = 0;
+let isAnswered = false, questionIdPrevious = 0, questionsAnswered = 0, correctAnswers = 0, score = 0;
 
 function getQuizzes() {
     const promise = axios.get(URL_API);
@@ -14,8 +14,6 @@ function showQuizzes(response) {
     if(ids === null) {
         ids = [];
     }
-
-    //use IDs to remove eventListeners
 
     for (let i = 0; i < response.data.length; i++){
         if(!ids.includes(response.data[i].id)) {
@@ -39,6 +37,7 @@ function showQuizzes(response) {
 
 }
 
+
 function openQuizzPage(image, title, questions) {
     const quizzesList = document.querySelector(".quizzes-list");
     const quizzPage = document.querySelector(".quizz-page");
@@ -52,16 +51,14 @@ function openQuizzPage(image, title, questions) {
         <span>${title}</span>
     </div>
     `
-
-    //Change to forEach
     
     for(let i = 0; i < questions.length; i++) {
-        quizzPage.innerHTML += `<section class="question">
+        quizzPage.innerHTML += `<section class="question" id="question${i + 1}">
         <div class="question-header">
             ${questions[i].title}
         </div>
 
-        <div class="question-items" id="question{${i + 1}}">
+        <div class="question-items">
         </div>
         </section>
         `
@@ -70,7 +67,7 @@ function openQuizzPage(image, title, questions) {
         question = document.querySelectorAll(".quizz-page .question .question-items")[i];
         for(let j = 0; j < questions[i].answers.length; j++){
             question.innerHTML += `    
-            <div class="question-item" onclick="selectAnswer(this, ${i+1});">
+            <div class="question-item correct-${questions[i].answers[j].isCorrectAnswer.toString()}" onclick="selectAnswer(this, ${i+1}, ${questions.length});">
                 <img src="${questions[i].answers[j].image}" alt="">
                 <p>${questions[i].answers[j].text}</p>
             </div>
@@ -98,27 +95,71 @@ function shuffle(array) {
     }
 }
 
-function selectAnswer(element, questionId) {
+function selectAnswer(element, questionId, questionsLength) {
     if(questionId !== questionIdPrevious) {
         isAnswered = false;
     }
 
-    if(!isAnswered) {
-        if(!element.classList.contains("incorrect-item")) {
-            element.classList.add("correct-item");
-        }
-        const items = element.parentNode.querySelectorAll(".question-item");
-        for (let i = 0; i < items.length; i++){
-            if(!items[i].classList.contains("correct-item")) {
-                items[i].classList.add("incorrect-item");
-            }
-        }
-        questionIdPrevious = questionId;
+    if(!isAnswered && questionsAnswered !== questionsLength) {
+        const answers = element.parentNode.querySelectorAll(".question-item");
+        element.classList.add("selected-answer");
 
+        if(element.classList.contains("correct-true")) {
+            correctAnswers += 1;
+        }
+
+        answers.forEach(function (answer) {
+            if(!answer.classList.contains("selected-answer")){
+                answer.classList.add("blur-item")
+            };
+
+            if(answer.classList.contains("correct-true")){
+                answer.classList.add("correct-item");
+            }
+            if(answer.classList.contains("correct-false")){
+                answer.classList.add("incorrect-item");
+            }
+
+        });
+
+        questionsAnswered += 1;
+        questionIdPrevious = questionId;
         isAnswered = true;
+        setTimeout(scrollToNextQuestion, 2000, questionId);
     }
 
+    showResult(questionsLength);
+}
+
+function scrollToNextQuestion (questionId) {
+    const nextDiv = document.querySelector(`#question${questionId + 1}`);
+    if (nextDiv !== null) {
+        nextDiv.scrollIntoView();
+    }
     
+}
+
+function showResult(questionsLength) {
+    if(questionsLength === questionsAnswered) {
+        score = correctAnswers/questionsLength;
+        const quizzPage = document.querySelector(".quizz-page");
+        quizzPage.innerHTML += `
+        <section class="result" id="question${questionsLength + 1}">
+            <div class="result-header">
+                88% de acerto: Você é praticamente um aluno de Hogwarts!
+            </div>
+            <div class="result-container">
+                <img src="assets/bruxo_chefe.png" alt="Bem-vindo a Hogwarts">
+                <span>Parabéns Potterhead! Bem-vindx a Hogwarts, aproveite o loop infinito de comida e clique no botão abaixo para usar o vira-tempo e reiniciar este teste.</span>
+            </div>
+        </section>
+        
+        <div class="container-buttons">
+            <button class="default-button restart-quizz-button">Reiniciar Quizz</button>
+            <p class="back-home-button">Voltar para home</p>
+        </div>
+        `
+    }
 }
 
 getQuizzes();
